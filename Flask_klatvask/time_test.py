0ea7225c86a8a12dd95e50de3b8356274024ce2b
dina2402 <116254339+dina2402@users.noreply.github.com>
2023-05-26 14:03:07 +0200

    Revert "Update machines"

--- Flask_klatvask/time_test.py
+++
@@ -1,8 +0,0 @@
-from datetime import date, time
-import datetime
-
-#print('ctime: ', date.ctime(date.today()))
-#print('today: ', date.today())
-#print(date.today(time.hour(), time.minute()))
-print(datetime.time())
-print(datetime.datetime(date.today()))
